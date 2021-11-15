d19d5775903b5c6eac01f9c2153008dcb180d9c0
Zulfikar Hikmatyar <89756828+flintcups@users.noreply.github.com>
2021-11-15 09:46:10 +0700

    Delete latihan3.py

--- latihan3.py
+++
@@ -1,4 +0,0 @@
-n_input = int(input('masukan banyak nama yang mau diinput = '))
-for i in range(0, n_input, +1):
-    nama = str(input('masukan nama anda = '))
-    print (nama,'(',len(nama),'karakter',')')
